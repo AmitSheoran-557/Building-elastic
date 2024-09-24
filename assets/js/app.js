30cd2b1b624ef5552c413aabb15d9602799cf9a5
AmitSheoran-557 <amit22082006@gmail.com>
2024-09-24 11:52:49 +0530

    setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,23 +13,3 @@ menuicon.addEventListener("click", function () {
     menubarThird.classList.toggle("-rotate-[40deg]");
     body.classList.toggle("overflow-lg-hidden")
 })
-
-// ===========================================
-document.addEventListener("DOMContentLoaded", function () {
-    const lazyImages = document.querySelectorAll('.lazy');
-
-    const imageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const img = entry.target;
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-                observer.unobserve(img);
-            }
-        });
-    });
-
-    lazyImages.forEach(image => {
-        imageObserver.observe(image);
-    });
-});
